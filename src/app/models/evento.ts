a7e7bd830bcb643f196ae539e25a036cba4e5e79
Ramón Gregori Esteve <rgregori@odec.es>
2026-06-26 13:25:51 +0200

    Nueva interface para evento

---
+++ src/app/models/evento.ts
@@ -0,0 +1,11 @@
+export interface Evento {
+  id: number;
+  fecha_evento: string;
+  descripcion: string;
+  lugar_evento: string;
+  distancia: number;
+  organizador: string;
+  precio: number;
+  url_info: string;
+  url_inscripcion: string;
+}
